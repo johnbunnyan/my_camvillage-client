083cea7093b81f487176711a93513eddca6f3bb3
sparklingwater226 <sparklingwater226@gmail.com>
2021-06-15 17:51:44 +0900

    :art: fix alter message issue

--- src/pages/Alter.js
+++ src/pages/Alter.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import axios from "axios";
 import { userAlter } from '../actions/index';
-import { withRouter, Route } from "react-router-dom";
+import { withRouter, Route, useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
 
-function Alter(props) {
-
+function Alter() {
+  const history = useHistory();
   const state = useSelector((state) => state)
-  console.log('Alter state = ', state)
+  // console.log('Alter state = ', state)
   
   const dispatch = useDispatch();
 
@@ -25,7 +25,7 @@ function Alter(props) {
     ErrorAll: '',
     ErrorPassword: '',
   })
-  const { ErrorAll, ErrorPassword} = errorInputs;
+  const { ErrorAll, ErrorPassword } = errorInputs;
 
   const onChange = (e) => {
     const {value, name} = e.currentTarget;
@@ -45,7 +45,7 @@ function Alter(props) {
 
   useEffect(() => {
     if (Password !== ConfirmPassword) {
-      handleError('ErrorPassword', '비밀번호, 비밀번호확인란이 일치하지 않습니다.')
+      handleError('ErrorPassword', '비밀번호가 일치하지 않습니다.')
     } else {
       handleError('ErrorPassword', '');
     }
@@ -53,15 +53,17 @@ function Alter(props) {
 
   const handleAlter = () => {
   
-    console.log('userId', UserId, 'password', Password)
+    console.log('userId', UserId, 'password', Password, 'nickname', NickName, 'Name', Name, 'Email', Email)
 
     const isTrue = UserId !== '' && Password !== '' &&
       NickName !== '' && Name !== '' && Email !== '';
 
-    // if (!isTrue) {
-    //   handleError('ErrorAll', '모든 항목을 입력하지 않았습니다.')
-    // } else {
+    if (!isTrue) {
+      handleError('ErrorAll', '모든 항목을 입력하지 않았습니다.')
+      console.log(errorInputs)
+    } else {
       handleError('ErrorAll', '')
+      console.log(errorInputs)
       axios
       .put('http://localhost:4000/user/alter',
       {
@@ -83,12 +85,12 @@ function Alter(props) {
       })
       .then((res) => {
         console.log('회원정보수정에 성공했습니다');
-        props.history.push('/user/mypage')
+        history.push('/user/mypage')
       })
       .catch((e) => {
         console.log(e)
       })
-   
+    }
     }
   
 
@@ -101,27 +103,30 @@ function Alter(props) {
       <div id='alter-info'>
         <div id='alter-name'>
           <div id='alter-name-label'>이름:</div>
-          <div id='alter-name-display'>{props.userInfo.name}</div>
+          <div id='alter-name-display'>{Name}</div>
         </div>
         <div>
-          <label htmlFor="alter-id">아이디:</label>
-          <input name="alter-id" defaultValue={UserId} onChange={onChange}></input>
+          <label htmlFor="UserId">아이디:</label>
+          <input name="UserId" defaultValue={UserId} onChange={onChange}></input>
         </div>
         <div>
-          <label htmlFor="alter-nickname">닉네임:</label>
-          <input name="alter-id" defaultValue={NickName} onChange={onChange}></input>
+          <label htmlFor="NickName">닉네임:</label>
+          <input name="NickName" defaultValue={NickName} onChange={onChange}></input>
         </div>
         <div>
-          <label htmlFor="alter-email">이메일:</label>
-          <input name="alter-id" defaultValue={Email} onChange={onChange}></input>
+          <label htmlFor="Email">이메일:</label>
+          <input name="Email" defaultValue={Email} onChange={onChange}></input>
         </div>
         <div>
-          <label htmlFor="alter-password">비밀번호:</label>
-          <input name="alter-id" onChange={onChange}></input>
+          <label htmlFor="Password">비밀번호:</label>
+          <input name="Password" onChange={onChange}></input>
         </div>
         <div>
-          <label htmlFor="alter-confirmpassword">비밀번호 확인:</label>
-          <input name="alter-id" onChange={onChange}></input>
+          <label htmlFor="ConfirmPassword">비밀번호 확인:</label>
+          <input name="ConfirmPassword" onChange={onChange}></input>
+        </div>
+        <div>
+          <button id='alter-btn' onClick={handleAlter}>수정하기</button>
         </div>
         <Route
           render={() => {
@@ -134,9 +139,6 @@ function Alter(props) {
             }
           }}
         />
-        <div>
-          <button id='alter-btn' onClick={handleAlter}>수정하기</button>
-        </div>
         <Route
           render={() => {
             if (ErrorAll !== '') {
